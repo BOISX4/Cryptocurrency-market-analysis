038c6ab366b49ca01674bb6161f1921a22798ab0
JeeBoomBoi <jeeboxical@gmail.com>
2021-11-13 10:56:20 +0400

    implemented chart view

--- pages/view-charts.js
+++ pages/view-charts.js
@@ -1,7 +1,27 @@
+import { createChart } from "lightweight-charts";
+import { useEffect, useRef } from "react";
+
 const ViewCharts = () => {
+    const chartRef = useRef(null);
+    const chartProperties = {
+        width : 800,
+        height :  700,
+    }
+    useEffect(() => {
+        const chart = createChart(chartRef.current, chartProperties);
+        const candleSeries = chart.addCandlestickSeries();
+        fetch(`https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&limit=1000`).then(res => res.json()).then(data => {
+	    const cdata = data.map(d => {
+		    return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
+	    })
+	    candleSeries.setData(cdata);
+        }).catch(err => console.log(err))
+    }, []);
+
     return (
         <div>
             <h1>View Charts</h1>
+            <div ref={chartRef} />
         </div>
     );
 }
