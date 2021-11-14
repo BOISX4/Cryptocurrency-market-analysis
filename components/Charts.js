8b801807b77f37cd9f89b4329b7426519f750c7d
JeeBoomBoi <jeeboxical@gmail.com>
2021-11-14 19:00:58 +0400

    added search feature for the chart

--- components/Charts.js
+++ components/Charts.js
@@ -1,5 +1,6 @@
 import { createChart } from "lightweight-charts";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Charts = () => {
     const chartRef = useRef(null);
@@ -7,10 +8,20 @@ const Charts = () => {
         width : 800,
         height : 700,
     }
+    const [pair, setPair] = useState('BTCUSDT');
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data, e) => {
+        setPair(data.pair);
+        console.log(data.pair, e);
+    }
+    const onError = (errors, e) => console.log(errors, e);
+    
+
     useEffect(() => {
+        //console.log("activates")
         const chart = createChart(chartRef.current, chartProperties);
         const candleSeries = chart.addCandlestickSeries();
-        fetch(`https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&limit=1000`).then(res => res.json()).then(data => {
+        fetch(`https://api.binance.com/api/v3/klines?symbol=${pair}&interval=1d&limit=1000`).then(res => res.json()).then(data => {
 	    const cdata = data.map(d => {
 		    return { 
                 time : d[0]/1000,
@@ -21,10 +32,16 @@ const Charts = () => {
 	        })
 	    candleSeries.setData(cdata);
         }).catch(err => console.log(err))
-    }, []);
+    }, [pair]);
 
     return (
-        <div ref={chartRef} />
+        <>
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
+            <label htmlFor="pair">Pair: </label>
+            <input {...register("pair")} />
+            <button type="submit">Submit</button>
+        </form>
+        <div ref={chartRef} /></>
     );
 }
  
