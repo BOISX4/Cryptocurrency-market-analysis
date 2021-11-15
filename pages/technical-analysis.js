de82c226df2441e30ed0d7e56674974d8d8786ba
JeeBoomBoi <jeeboxical@gmail.com>
2021-11-15 23:32:31 +0400

    added frontend for technical indicators

--- pages/technical-analysis.js
+++ pages/technical-analysis.js
@@ -9,23 +9,15 @@ const SMA = require('technicalindicators').SMA;
 //     const res = await fetch()
 // }
 
-async function asyncCall() {
-    let data = await fetch(`https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=100`)
+async function asyncCall(pair) {
+    let data = await fetch(`https://api.binance.com/api/v3/klines?symbol=${pair}&interval=1d&limit=180`)
     .then(res => res.json())
     .then(data => {
         const cdata = data.map(d => {
             return { 
-                open : parseFloat(d[1]),
-                high : parseFloat(d[2]),
-                low : parseFloat(d[3]),
                 close : parseFloat(d[4])}
-        })
-        let open = getData("open", cdata);
-        let high = getData("high", cdata);
-        let low = getData("low", cdata);
-        let close = getData("close", cdata);
-        let d = [open, high, low, close]
-        return d;
+            })
+        return(getData("close", cdata));
     })
     .catch(err => console.log(err))
     return(data);
@@ -40,8 +32,10 @@ function getData(name, data) {
 }
 
 function calculateMACD(res) {
+    let id = 0;
+    let key, score;
     const macdInput = {
-        values            : res[3],
+        values            : res,
         fastPeriod        : 12,
         slowPeriod        : 26,
         signalPeriod      : 3 ,
@@ -53,59 +47,127 @@ function calculateMACD(res) {
     const last_macd_value = last_value['MACD'];
     const last_signal_value = last_value['signal'];
     if (last_macd_value < last_signal_value) {
-        return {macd: last_macd_value.toFixed(2), score: -1};
+        key = last_macd_value.toFixed(2); 
+        score = -1;
     } else {
-        return {macd: last_macd_value.toFixed(2), score: 1};
+        key = last_macd_value.toFixed(2), 
+        score = 1;
+    }
+    return {
+        id: id,
+        key: key,
+        score: score
     }
 }
 
 function calculateRSI(res) {
+    let id = 1;
+    let key, score;
     const inputRSI = {
-        values: res[3],
+        values: res,
         period: 14
     }
     const RSI_values = RSI.calculate(inputRSI);
     const last_value = (RSI_values[RSI_values.length -1]);
     if (last_value >= 70) {
-        return {rsi: last_value, score: 1};
+        key = last_value
+        score = 1
     } else if (last_value < 30) {
-        return {rsi: last_value, score: -1};
+        key = last_value 
+        score = -1
     } else {
-        return {rsi: last_value, score: 0};
+        key = last_value
+        score = 0
+    }
+    return {
+        id: id,
+        key: key,
+        score: score
     }
 }
 
 function calculateEMA(res) {
+    let id = 2;
+    let key, score;
     let period = 8;
-    let values = res[3];
+    let values = res;
     const EMA_values = EMA.calculate({period: period, values: values});
     const last_value = EMA_values[EMA_values.length - 1];
-    return last_value.toFixed(2);
+    return {
+        id : id,
+        key: last_value.toFixed(2), 
+        score: 0
+    };
 }
 
 function calculateSMA(res) {
+    let id = 3;
+    let key, score;
     let period = 8;
-    let values = res[3];
+    let values = res;
     const SMA_values = SMA.calculate({period: period, values: values});
     const last_value = SMA_values[SMA_values.length - 1];
-    return last_value.toFixed(2);
+    return {
+        id: id,
+        key: last_value.toFixed(2), 
+        score: 0
+    };
 }
 
 const TechAnalysis = () => {
-    let data = asyncCall();
-    data = data.then(res => {
+    const [pair, setPair] = useState('BTCUSDT');
+    const [result, setResult] = useState([]);
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data, e) => {
+        setPair(data.pair);
+        let cdata = asyncCall(pair);
+        cdata = cdata.then(res => {
         //console.log(res);
         const macd_result = calculateMACD(res);
         const rsi_result = calculateRSI(res);
         const ema_result = calculateEMA(res);
         const sma_result = calculateSMA(res);
-        console.log({ macd_result }, { rsi_result }, ema_result, sma_result);
-    });
+        setResult([macd_result, rsi_result, ema_result, sma_result]);       
+        //console.log(result); 
+        });
+    }
+    const onError = (errors, e) => console.log(errors, e);
+    //console.log(result);
     return (  
         <div>
             <h1>Technical Analysis</h1>
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
+                <label htmlFor="pair">Pair: </label>
+                <input {...register("pair")} />
+                <button type="submit">Search</button>
+            </form>
+            <table>
+            <thead>
+            <tr>
+                <th>NAME</th>
+                <th>MACD</th>
+                <th>RSI</th>
+                <th>EMA</th>
+                <th>SMA</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>
+                    {pair}
+                </td>
+                {result.map(res => (
+                <td key={res.id}>
+                    {res.key}
+                </td>
+                ))}
+            </tr>
+            </tbody>
+            </table>
         </div>
     );
 }
  
 export default TechAnalysis;
+
+{/* {result.map(res => <p key="{key.id}">{res.key}</p>)} */}
